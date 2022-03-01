1515066ac597cbba28290c943032df2f3d6c3a65
FernandoBade <bade.fernando@gmail.com>
2022-02-28 22:19:08 -0300

    Finalização da estrutura JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function input(numero) {
+    let numeros = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = numeros + numero;
+}
+
+function clean() {
+    document.getElementById("resultado").innerHTML = ""
+}
+
+function deletar() {
+    let resultado = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    let resultado = document.getElementById("resultado").innerHTML;
+    if(resultado){
+        document.getElementById("resultado").innerHTML = eval(resultado).toFixed(2);
+    } else{
+        document.getElementById("resultado").innerHTML = "( ͡° ʖ̯ ͡°)   rly?";
+    }
+}
+
